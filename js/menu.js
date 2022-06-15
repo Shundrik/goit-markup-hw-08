653fa0fcd8f96d406d43bbff797cefd43db89692
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-06-15 18:34:04 +0300

    hw-8

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,12 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  // const bodyRef = document.body.classList.toggle("is-open");
+ 
+  // document.body.classList.toggle("modal-open");
+  const bodyRef = document.querySelector("[is-open]")
+
+
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -8,7 +14,18 @@
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
+    
 
     mobileMenuRef.classList.toggle("is-open");
+    bodyRef.classList.toggle("is-open");
+  
+    // const bodyRef = document.body.classList.add("is-open");
+    // const bodyRef = document.body.classList.toggle()
+  
   });
+  
+  
+
+
+
 })();
